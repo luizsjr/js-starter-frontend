3f36d8bd9c310946569aac340629abd8256e7a14
Luiz Souza <luiz.l.souza@ericsson.com>
2018-04-10 16:50:16 -0700

    eslint setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import 'bootstrap'
 import './scss/index.scss'
-import fontawesome from '@fortawesome/fontawesome';
+import {library} from '@fortawesome/fontawesome'
 import faSearch from '@fortawesome/fontawesome-free-solid/faSearch';
 import faChevronRight from '@fortawesome/fontawesome-free-solid/faChevronRight';
 
-//Font Awesome Search Icon
-fontawesome.library.add(faSearch, faChevronRight);
+// Font Awesome Search Icon
+library.add(faSearch, faChevronRight);
 
-//Change navbar active
-$(".nav-link").on("click", function() {
+// Change navbar active
+$('.nav-link').click( (event) => {
   $('li.active').removeClass('active');
-  $(this).closest('li').addClass('active');
+  $(event.currentTarget).closest('li').addClass('active');
 });
